c910daf5b5f83c020f63d57ec3b4d2257d1e4266
Tania Allard <taniar.allard@gmail.com>
2021-11-09 15:24:53 +0000

    :construction_worker: Add HTML proofer

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,13 @@
 from nox import session
 
+# Nox sessions are defined here, if you are running these locally you might want
+# to run each session individually, for example
+# nox --sessions docs
+
 
 @session(reuse_venv=True)
 def docs(session):
+    """Docs session for CI purposes - it installs the needed dependencies
+    and build the JupyterBook"""
     session.install("jupyter-book")
-    session.run("jb", "build", "docs", "--path-output=.",
-                "--builder=singlehtml")
+    session.run("jb", "build", "docs", "--path-output=.", "--builder=singlehtml")
